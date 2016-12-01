971a287bdc0f3cec114fbe132eae5ac4249e6fd1
Ricky Maisuria <ricky@rickys-mbp.home>
2016-12-01 02:23:13 +0000

    Completed email auth

--- spark/SignInVC.swift
+++ spark/SignInVC.swift
@@ -13,6 +13,9 @@ import Firebase
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var pwdField: FancyField!
+    @IBOutlet weak var emailField: FancyField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -57,6 +60,24 @@ class SignInVC: UIViewController {
                 print("JESS: Successfully authenticated with Firebase")
             }
         })
+    }
+    
+    @IBAction func signInTapped(_ sender: Any) {
+        if let email = emailField.text, let pwd = pwdField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                if error == nil {
+                    print("JESS: Email user authenticated with Firebase")
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("JESS: Unable to authenticate with Firebase using email")
+                        } else {
+                            print("JESS: Successfully authenticated with Firebase")
+                        }
+                    })
+                }
+            })
+        }
     }
 
 }
