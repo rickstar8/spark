667ffcbe9dd2615f85751ecce1665f744c226880
Ricky Maisuria <ricky@rickys-mbp.home>
2016-12-04 03:27:10 +0000

    Firebase get complete

--- spark/FeedVC.swift
+++ spark/FeedVC.swift
@@ -14,6 +14,8 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,7 +24,23 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
             
-            print(snapshot.value)
+            self.posts = []
+            
+            if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                
+                for snap in snapshot {
+                    
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+                
+            }
+            
+            self.tableView.reloadData()
             
         })
     }
@@ -32,11 +50,16 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return (tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell)!
+        
+        let post = posts[indexPath.row]
+        print("JESS: \(post.caption)")
+        
+        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
     
     @IBAction func signOutTapped(_ sender: Any) {
