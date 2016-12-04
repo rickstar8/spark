bcc9e7cdb2338680e277779a8928971e94af8808
Ricky Maisuria <ricky@rickys-mbp.home>
2016-12-04 01:30:37 +0000

    Feed table complete

---
+++ spark/PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostCell.swift
+//  spark
+//
+//  Created by Ricky Maisuria on 03/12/2016.
+//  Copyright © 2016 Ricky Maisuria. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+    
+
+}
