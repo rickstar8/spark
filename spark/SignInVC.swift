d9b96db47d7013938ba6a7d2242045b3ec5d62e6
Ricky Maisuria <ricky@rickys-mbp.home>
2016-12-04 02:25:44 +0000

    Completed creating Firebase DB users

--- spark/SignInVC.swift
+++ spark/SignInVC.swift
@@ -67,7 +67,8 @@ class SignInVC: UIViewController {
             } else {
                 print("JESS: Successfully authenticated with Firebase")
                 if let user = user {
-                    self.completeSignIn(id: user.uid)
+                    let userData = ["provider": credential.provider]
+                    self.completeSignIn(id: user.uid, userData: userData)
                 }
             }
         })
@@ -79,7 +80,8 @@ class SignInVC: UIViewController {
                 if error == nil {
                     print("JESS: Email user authenticated with Firebase")
                     if let user = user {
-                        self.completeSignIn(id: user.uid)
+                        let userData = ["provider": user.providerID]
+                        self.completeSignIn(id: user.uid, userData: userData)
                     }
                 } else {
                     FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
@@ -88,7 +90,8 @@ class SignInVC: UIViewController {
                         } else {
                             print("JESS: Successfully authenticated with Firebase")
                             if let user = user {
-                                self.completeSignIn(id: user.uid)
+                                let userData = ["provider": user.providerID]
+                                self.completeSignIn(id: user.uid, userData: userData)
                             }
                         }
                     })
@@ -97,7 +100,8 @@ class SignInVC: UIViewController {
         }
     }
     
-    func completeSignIn(id: String) {
+    func completeSignIn(id: String, userData: Dictionary<String, String>) {
+        DataService.ds.createFirebaseDBUser(uid: id, userData: userData)
         let keychainResult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
         print("JESS: Data saved to keychain \(keychainResult)")
         performSegue(withIdentifier: "goToFeed", sender: nil)
