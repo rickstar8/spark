835b5cf859a850917713dc1520a7ced6949619ce
Ricky Maisuria <ricky@rickys-mbp.home>
2016-12-04 15:57:12 +0000

    Firebase storage download complete

--- spark/DataService.swift
+++ spark/DataService.swift
@@ -10,14 +10,19 @@ import Foundation
 import Firebase
 
 let DB_BASE = FIRDatabase.database().reference()
+let STORAGE_BASE = FIRStorage.storage()
 
 class DataService {
     
     static let ds =  DataService()
     
+    // DB References
     private var _REF_BASE = DB_BASE
     private var _REF_POSTS = DB_BASE.child("posts")
     private var _REF_USERS = DB_BASE.child("users")
+    
+    // Storage References
+    private var _REF_POSTS_IMAGES = STORAGE_BASE.reference().child("post-pics")
     
     var REF_BASE: FIRDatabaseReference {
         return _REF_BASE
@@ -30,6 +35,12 @@ class DataService {
     var REF_USERS: FIRDatabaseReference {
         return _REF_USERS
     }
+    
+    var REF_POSTS_IMAGES: FIRStorageReference {
+        return _REF_POSTS_IMAGES
+    }
+    
+    
     
     func createFirebaseDBUser(uid: String, userData: Dictionary<String, String>) {
         REF_USERS.child(uid).updateChildValues(userData)
